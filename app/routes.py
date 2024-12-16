7ec84787b77f59740521f3f5d0be29069cc500bb
algracevian <agresvian12345@gmail.com>
2024-12-16 17:49:20 +0700

    test backup

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, current_app
 
 main = Blueprint('main', __name__)
 
@@ -7,13 +7,59 @@ main = Blueprint('main', __name__)
 def home():
     return render_template('home.html')
 
-# List Buku
+# List Books (Read)
 @main.route('/books')
 def books():
-    # Contoh data buku
-    book_list = [
-        {"title": "Buku 1", "author": "Penulis A", "year": 2020},
-        {"title": "Buku 2", "author": "Penulis B", "year": 2021},
-        {"title": "Buku 3", "author": "Penulis C", "year": 2022},
-    ]
-    return render_template('books.html', books=book_list)
+    cur = current_app.mysql.connection.cursor()  # Access MySQL connection from current_app
+    cur.execute("SELECT * FROM books")
+    books = cur.fetchall()
+    cur.close()
+    return render_template('books.html', books=books)
+
+# Create Book (Create)
+@main.route('/books/add', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        year = request.form['year']
+
+        cur = current_app.mysql.connection.cursor()  # Access MySQL connection from current_app
+        cur.execute("INSERT INTO books (title, author, year) VALUES (%s, %s, %s)", (title, author, year))
+        current_app.mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('main.books'))
+
+    return render_template('add_book.html')
+
+# Edit Book (Update)
+@main.route('/books/edit/<int:id>', methods=['GET', 'POST'])
+def edit_book(id):
+    cur = current_app.mysql.connection.cursor()  # Access MySQL connection from current_app
+    cur.execute("SELECT * FROM books WHERE id = %s", [id])
+    book = cur.fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        year = request.form['year']
+
+        cur.execute("UPDATE books SET title = %s, author = %s, year = %s WHERE id = %s", (title, author, year, id))
+        current_app.mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('main.books'))
+
+    cur.close()
+    return render_template('edit_book.html', book=book)
+
+# Delete Book (Delete)
+@main.route('/books/delete/<int:id>')
+def delete_book(id):
+    cur = current_app.mysql.connection.cursor()  # Access MySQL connection from current_app
+    cur.execute("DELETE FROM books WHERE id = %s", [id])
+    current_app.mysql.connection.commit()
+    cur.close()
+
+    return redirect(url_for('main.books'))
